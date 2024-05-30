b019fd0abb49187614fc729271991a4ba4370de8
benj-h <bheto052@uottawa.ca>
2024-05-30 16:10:29 -0400

    renamed MainActivity to LoginActivity

--- app/src/main/java/ca/uottawa/team3/rentron/LoginActivity.java
+++ app/src/main/java/ca/uottawa/team3/rentron/LoginActivity.java
@@ -1,10 +1,6 @@
 package ca.uottawa.team3.rentron;
 
 import android.os.Bundle;
-import android.widget.Button;
-import android.widget.EditText;
-import android.view.View;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
@@ -16,15 +12,13 @@ import androidx.core.view.WindowInsetsCompat;
 
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.util.HashMap;
 import java.util.Map;
 
-public class MainActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity {
 
     FirebaseFirestore firestore;
 
@@ -32,7 +26,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
